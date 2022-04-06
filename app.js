782046c981e1c2751dc6cb24fa3497fa67695beb
1ptqc-KD-97bsBG-pVJG <76831037+1ptqc-KD-97bsBG-pVJG@users.noreply.github.com>
2022-04-06 18:31:36 -0400

    Began work towards drawing tasks from objects,  gpt-j API
    Currently, tasks are drawn from an array called taskNames, which is created from an array of objects called tasks. Working towards skipping taskNames step so drawn tasks can be identified with ID, priority, etc. Also, successfully implemented a button that tells the users a generated joke using gpt-j (this is likely temporary and will be used for a different project).

--- app.js
+++ app.js
@@ -12,15 +12,120 @@ window.addEventListener('load', () => {
     const input = document.querySelector("#new-task-input");
     const delete_all = document.querySelector("#delete_all");
     const print_all = document.querySelector("#print_all");
+    const draw_tasks = document.querySelector("#drawTasks_button");
     let taskNames = [];
     let tasks = [];    
 
     
     function drawTasks() {
+       // old version of drawTasks (worked)
+        // // refresh task list based on local storage
+        // try {
+        //     //tasks = JSON.parse(localStorage.tasks);
+        //     taskNames = JSON.parse(localStorage.tasks).map(a => a.taskName)
+        //     //console.log(testTasks);
+        // } catch (e) {
+        //     taskNames = [];
+        // }
+        
+        // // delete all drawn tasks
+        // try {
+        //     const tempTasks = document.querySelectorAll('.task');
+        //     tempTasks.forEach((drawnTask) => {
+        //         drawnTask.remove();
+        //     })
+        // } catch (e) {
+        //     console.log("Failed to delete drawn tasks.")
+        //     console.log(e)
+        // }
+        
+        // // draw tasks
+        // try {
+        //     taskNames.forEach((task) => {
+        //         const list_el = document.querySelector("#tasks");
+                
+        //         // creates tasks
+        //         const task_el = document.createElement("div");
+        //         task_el.classList.add("task");
+        
+        //         const task_content_el = document.createElement("div");
+        //         task_content_el.classList.add("content");
+        
+        //         task_el.appendChild(task_content_el);
+        
+        //         const task_input_el = document.createElement("input");
+        //         task_input_el.classList.add("text");
+        //         task_input_el.type = "text";
+        //         // determines the displayed text
+        //         task_input_el.value = task;
+        //         task_input_el.setAttribute("readonly", "readonly");
+        
+        //         task_content_el.appendChild(task_input_el);
+        
+        //         // creates buttons (required to display)
+        //         const task_actions_el = document.createElement("div");
+        //         task_actions_el.classList.add("actions");
+        
+        //         const task_edit_el = document.createElement("button");
+        //         task_edit_el.classList.add("edit");
+        //         task_edit_el.innerHTML = "Edit";
+        
+        //         const task_delete_el = document.createElement("button");
+        //         task_delete_el.classList.add("delete");
+        //         task_delete_el.innerHTML = "Delete";
+        
+        //         task_actions_el.appendChild(task_edit_el);
+        //         task_actions_el.appendChild(task_delete_el);
+        
+        //         task_el.appendChild(task_actions_el);
+        
+        //         list_el.appendChild(task_el);
+
+        //         // edit button functionality
+        //         task_edit_el.addEventListener('click', () => {
+        //             console.log("edit clicked")
+        //             console.log("selected task:")
+        //             console.log(task_el)
+        //             if (task_edit_el.innerText.toLowerCase() == "edit") {
+        //                 task_input_el.removeAttribute("readonly");
+        //                 // need to look into .focus
+        //                 task_input_el.focus();
+        //                 task_edit_el.innerText = "Save";
+        //             } else {
+        //                 task_input_el.setAttribute("readonly", "readonly");
+        //                 task_edit_el.innerText = "Edit";
+        //             }
+        //         });
+
+        //         // delete button functionality
+        //         task_delete_el.addEventListener('click', () => {
+        //                 console.log("here")
+        //                 console.log(task_el)
+        //                 taskNames.remove(task)
+        //                 list_el.removeChild(task_el);
+        //             });
+        //     })
+    
+        // } catch (e) {
+        //     console.log("failed to draw tasks, error:");
+        //     console.log(e);
+        // }
+        
+        //=======
+        // new version
+        
         // refresh task list based on local storage
         try {
             //tasks = JSON.parse(localStorage.tasks);
             taskNames = JSON.parse(localStorage.tasks).map(a => a.taskName)
+            tasks = [];
+            localStorage.tasks.forEach((task) => {
+                tasks.push(task);
+            });
+            console.log("session tasks array:")
+            console.log(tasks)
+            console.log("local storage tasks")
+            console.log(localStorage.tasks)
             //console.log(testTasks);
         } catch (e) {
             taskNames = [];
@@ -39,7 +144,8 @@ window.addEventListener('load', () => {
         
         // draw tasks
         try {
-            taskNames.forEach((task) => {
+            console.log(tasks)
+            tasks.forEach((task) => {
                 const list_el = document.querySelector("#tasks");
                 
                 // creates tasks
@@ -55,7 +161,7 @@ window.addEventListener('load', () => {
                 task_input_el.classList.add("text");
                 task_input_el.type = "text";
                 // determines the displayed text
-                task_input_el.value = task;
+                task_input_el.value = task.taskName;
                 task_input_el.setAttribute("readonly", "readonly");
         
                 task_content_el.appendChild(task_input_el);
@@ -82,6 +188,8 @@ window.addEventListener('load', () => {
                 // edit button functionality
                 task_edit_el.addEventListener('click', () => {
                     console.log("edit clicked")
+                    console.log("selected task:")
+                    console.log(task_el)
                     if (task_edit_el.innerText.toLowerCase() == "edit") {
                         task_input_el.removeAttribute("readonly");
                         // need to look into .focus
@@ -145,17 +253,17 @@ window.addEventListener('load', () => {
         drawTasks();
 
         // edit button funcitonality
-        task_edit_el.addEventListener('click', () => {
-            console.log("edit clicked")
-            if (task_edit_el.innerText.toLowerCase() == "edit") {
-                task_input_el.removeAttribute("readonly");
-                task_input_el.focus();
-                task_edit_el.innerText = "Save";
-            } else {
-                task_input_el.setAttribute("readonly", "readonly");
-                task_edit_el.innerText = "Edit";
-            }
-        });
+        // task_edit_el.addEventListener('click', () => {
+        //     console.log("edit clicked")
+        //     if (task_edit_el.innerText.toLowerCase() == "edit") {
+        //         task_input_el.removeAttribute("readonly");
+        //         task_input_el.focus();
+        //         task_edit_el.innerText = "Save";
+        //     } else {
+        //         task_input_el.setAttribute("readonly", "readonly");
+        //         task_edit_el.innerText = "Edit";
+        //     }
+        // });
 
     });
     
@@ -185,12 +293,18 @@ window.addEventListener('load', () => {
     
     print_all.addEventListener('click', () => {
         try {
+            console.log("session tasks array:")
+            console.log(tasks)
             console.log("local storage:")
             console.log(JSON.parse(localStorage.tasks))
             console.log("========")
         } catch (e) {
             console.log("No tasks in local storage.")
         }
+    });
+
+    draw_tasks.addEventListener('click', () => {
+        drawTasks();
     });
 
 });
@@ -206,6 +320,61 @@ document.addEventListener('keydown', function(event){
 	}
 });
 
-function toggleDuplicates () {
+function toggleDuplicates() {
     console.log("user wants to toggle duplicates!")
+}
+
+
+// testing gpt-j fetch request
+let returnedText = "";
+function testAPI() {
+    console.log("this should hold response:")
+    fetch("https://api.ai21.com/studio/v1/j1-jumbo/complete", {
+      headers: {
+        "Authorization": "Bearer X9U5ZKe7tI7oe5ni0zPLyIUo3w19xJCS",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+          "prompt": "Joke: ",
+          "numResults": 1,
+          "maxTokens": 64,
+          "temperature": 0.7,
+          "topKReturn": 0,
+          "topP":1,
+          "countPenalty": {
+            "scale": 0,
+            "applyToNumbers": false,
+            "applyToPunctuations": false,
+            "applyToStopwords": false,
+            "applyToWhitespaces": false,
+            "applyToEmojis": false
+          },
+          "frequencyPenalty": {
+            "scale": 0,
+            "applyToNumbers": false,
+            "applyToPunctuations": false,
+            "applyToStopwords": false,
+            "applyToWhitespaces": false,
+            "applyToEmojis": false
+          },
+          "presencePenalty": {
+            "scale": 0,
+            "applyToNumbers": false,
+            "applyToPunctuations": false,
+            "applyToStopwords": false,
+            "applyToWhitespaces": false,
+            "applyToEmojis": false
+          },
+          "stopSequences":["\n"]
+        }),
+      method: "POST"
+    }).then(response => response.json()).then(data => {
+        returnedText = data;
+        printResponse();
+    });
+}
+
+function printResponse() {
+    console.log(returnedText)
+    alert(returnedText.completions[0].data.text)
 }
